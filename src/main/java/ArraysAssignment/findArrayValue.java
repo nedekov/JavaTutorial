f593ae1f6dd744d2ad5c4a6f6a0edf8caa720dd4
nedekov <ailqka@gmail.com>
2021-10-31 17:57:36 +0200

    Update findArrayValue.java
    Happy helloween :D

--- src/main/java/ArraysAssignment/findArrayValue.java
+++ src/main/java/ArraysAssignment/findArrayValue.java
@@ -2,6 +2,25 @@ package ArraysAssignment;
 
 //4. Write a Java program to test if an array contains a specific value.
 
+import java.util.Scanner;
+
 public class findArrayValue {
     public static void main (String[] args){
-}}
+
+        int[] intArray = new int[]{1, 2, 3, 4, 5,6,7,8,9,10};
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter the value you're searching for: ");
+        boolean found = false;
+        int searchFor = in.nextInt();
+
+        for(int x : intArray){
+            if(x == searchFor){
+                found = true;
+                break;
+            }
+        }
+
+        System.out.println("The value you've searched for is present in the array: " +found);
+
+}
+}
